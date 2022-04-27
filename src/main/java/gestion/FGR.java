892ef0007578a12712d50f4b194cddf566e34410
Mateo Cañavera <mcanavera@unal.edu.co>
2022-04-26 21:26:29 -0500

    Update FGR.java

--- src/main/java/gestion/FGR.java
+++ src/main/java/gestion/FGR.java
@@ -6,8 +6,9 @@ public class FGR {
 	public static void main(String[] args) {
 		
 		Animal animal = new Animal ("Herme",10,"rora","M");
-		System.out.println(animal);
 		
+		
+		System.out.println(Animal.totalPorTipo());
 	
 	}
 
