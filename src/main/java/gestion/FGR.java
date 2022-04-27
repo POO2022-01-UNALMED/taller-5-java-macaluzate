b8dcd76db81bcad7d7ac909ce991fc751ab1a13e
Mateo Cañavera <mcanavera@unal.edu.co>
2022-04-26 21:00:52 -0500

    1

---
+++ src/main/java/gestion/FGR.java
@@ -0,0 +1,15 @@
+package gestion;
+import zooAnimales.Animal;
+
+public class FGR {
+	
+	public static void main(String[] args) {
+		
+		Animal animal = new Animal ("Herme",10,"rora","M");
+		System.out.println(animal);
+		
+	
+	}
+
+}
+ 
